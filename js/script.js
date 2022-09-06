d37b9dc3e92a5224806ee68e444b942057f3e1c3
rav4353 <ravanthsri20@gmail.com>
2022-09-06 10:20:59 +0530

    hi

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var n = 456;
+let temps = n;
+let z = 0;
+
+while(n>0) {
+    let rem = n % 10;
+    z = (z) + (rem*rem*rem);
+    n = Math.floor(n/10);
+}
+
+if (temps == z) {
+    console.log("its an amstrong num");
+}
+else{
+    console.log("its not an amstrong num");
+}
+
